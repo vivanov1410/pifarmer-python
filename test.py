025cfee956c82c2e42bdc8b3f45c6467526f118b
Viacheslav Ivanov <slava.ivanov@fyidoctors.com>
2014-08-25 19:39:59 -0700

    added other device funcs

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ import pifarm_device
 
 device = pifarm_device.init()
 
-print('general.uptime: {}'.format(device.general.uptime()))
+print('general.uptime: {}'.format(device.general.uptime))
 print('general.processes: {}'.format(device.general.processes()))
 print()
 print('cpu.temperature: {}'.format(device.cpu.temperature()))
 print('cpu.speed: {}'.format(device.cpu.speed()))
 print()
 print('gpu.temperature: {}'.format(device.gpu.temperature()))
-#print('gpu.speed: {}'.format(device.gpu.speed()))
+print()
+print('network.ip: {}'.format(device.network.ip()))
+print('network.connections: {}'.format(device.network.connections()))
